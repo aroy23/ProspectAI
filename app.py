ecb2d5d1a929d8a10ff18f0b2cda6637d6a91d32
ssha27 <stsha2023@gmail.com>
2024-10-12 12:49:35 -0700

    initial flask server

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, 
+            static_folder="../frontend/assets/css",
+            template_folder="../frontend/assets/js")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
