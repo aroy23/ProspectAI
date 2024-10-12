d0316a405a6579c4759639a295bcba1e31362c11
Choidorj Bayarkhuu <choidorj@Choidorjs-MacBook-Pro.local>
2024-10-12 14:56:09 -0700

    initial commit in the branch

--- backend/api.py
+++ backend/api.py
@@ -1,49 +1,16 @@
-import requests
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play, stream, save
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
-# Replace with your Twelve Labs API key
-API_KEY = ""
-video_file = "test.mp4"
-
-# Define the headers for authentication
-headers = {
-"Authorization": f"Bearer {API_KEY}",
-"Content-Type": "application/json",
-}
-
-# Upload the video to Twelve Labs
-with open(video_file, 'rb') as f:
-	response = requests.post(
-		"https://api.twelvelabs.io/videos", 
-		headers=headers,
-		files={"file": f}
-	)
-
-if response.status_code == 200:
-	video_id = response.json().get("id")
-	print(f"Video uploaded successfully. Video ID: {video_id}")
-else:
-	print("Failed to upload video", response.text)
-	
-transcription_endpoint = f"https://api.twelvelabs.io/videos/{video_id}/transcribe"
-
-transcription_response = requests.post(
-	transcription_endpoint,
-	headers=headers
+myAPI_KEY = os.getenv("ELEVENLABS_API_KEY")
+client = ElevenLabs(
+  api_key=myAPI_KEY,
 )
-
-if transcription_response.status_code == 200:
-	transcription = transcription_response.json()
-	print("Transcription:", transcription)
-else:
-	print("Failed to transcribe video", transcription_response.text)
-# Check transcription status
-status_response = requests.get(
-	transcription_endpoint,
-	headers=headers
+audio = client.generate(
+  text="Lebron James is the best basketball player but Micheal Jordan was the actual genuine Greatest of All Time.",
+  voice="Rachel",
+  model="eleven_monolingual_v1"
 )
-
-if status_response.status_code == 200:
-	transcript_text = status_response.json().get("transcript")
-	print("Transcription completed:", transcript_text)
-else:
-	print("Transcription not ready yet.")
+save(audio, "my-file.mp3")
