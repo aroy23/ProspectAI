8a77b39288b6e7b70eb694ff2af453bfea787862
ssha27 <stsha2023@gmail.com>
2024-10-12 20:42:59 -0700

    app changes

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,11 @@ app = Flask(__name__,
             static_folder="../frontend/assets/css",
             template_folder="../frontend/assets/js")
 
-# Define the folder to store uploaded files
 UPLOAD_FOLDER = './uploads'  # Adjust this path as needed
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'mkv'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Helper function to check allowed file extensions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -36,26 +34,6 @@ def login(username, password):
         return db_login(username, password)
     except TypeError as e:
         return jsonify({'error': 'Enter both email and password!'})
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part in the request'}), 400
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file and allowed_file(file.filename):
-        filename = file.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
-        return jsonify({'message': 'File uploaded successfully', 'filepath': filepath}), 200
-
-    return jsonify({'error': 'File type not allowed'}), 400
-
-
 
 if __name__ == '__main__':
     # Create upload folder if it doesn't exist
