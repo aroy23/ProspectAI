b546587a01b9b47b394fcac9c4c486580d9fc685
ssha27 <stsha2023@gmail.com>
2024-10-13 00:40:17 -0700

    Adding Routes

--- backend/app.py
+++ backend/app.py
@@ -18,31 +18,52 @@ def allowed_file(filename):
 def home():
     return render_template("index.html")
 
-@app.route('/add_user')
-def add_user(username, email, password):
-    try:
-        db_add(username, email, password)
-    except sqlite3.IntegrityError as e:
-        if e == 'UNIQUE constraint failed: user_data.username':
-            return jsonify({'error': 'Username already in use!'})
-        elif e == 'UNIQUE constraint failed: user_data.email':
-            return jsonify({'error': 'Email already in use!'})
+# @app.route('/add_user')
+# def add_user(username, email, password):
+#     try:
+#         db_add(username, email, password)
+#     except sqlite3.IntegrityError as e:
+#         if e == 'UNIQUE constraint failed: user_data.username':
+#             return jsonify({'error': 'Username already in use!'})
+#         elif e == 'UNIQUE constraint failed: user_data.email':
+#             return jsonify({'error': 'Email already in use!'})
 
 @app.route('/login_page', methods=['GET'])
 def login_page():
     return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def login(username, password):
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
     try:
-        return db_login(username, password)
+        if db_login(username, password):
+            return render_template('homepage.html')
+        else:
+            return render_template('login.html')
     except TypeError as e:
         return jsonify({'error': 'Enter both email and password!'})
     
 @app.route('/signup_page', methods=['GET'])
 def signup_page():
     return render_template('signup.html')
-    
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        db_add(username, email, password)
+        return render_template('login.html')
+    except sqlite3.IntegrityError and TypeError as e:
+        if e == 'UNIQUE constraint failed: user_data.email':
+            return jsonify({'error': 'Email is already in use!'})
+        elif e == 'UNIQUE constraint failed: user_data.username':
+            return jsonify({'error': 'Username is already in use!'})
+        else:
+            return jsonify({'error': e})
+
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
     return 0
